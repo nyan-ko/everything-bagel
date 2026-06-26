49ea33b2feed946ccf6ca9c8986d6499a01093cc
Edric Liu <edricdongliu@gmail.com>
2026-06-26 16:37:58 -0400

    add tag datagen

--- src/main/java/com/nyan/everybagel/ModItemTags.java
+++ src/main/java/com/nyan/everybagel/ModItemTags.java
@@ -6,7 +6,7 @@ import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.Item;
 
 public class ModItemTags {
-    public static final TagKey<Item> MOD_DEBUG = create(EverythingBagel.MOD_ID + "_debug");
+    public static final TagKey<Item> MOD_DEBUG = create("debug");
 
     public static final TagKey<Item> MIXING_BOWL_INPUT = create("mixing_bowl_input");
 
--- src/main/java/com/nyan/everybagel/datagen/ModItemTagProvider.java
+++ src/main/java/com/nyan/everybagel/datagen/ModItemTagProvider.java
@@ -1,9 +1,12 @@
 package com.nyan.everybagel.datagen;
 
 import com.nyan.everybagel.EverythingBagel;
+import com.nyan.everybagel.ModItemTags;
+import com.nyan.everybagel.items.ModItems;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.tags.ItemTagsProvider;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.level.block.Block;
 import net.neoforged.neoforge.common.data.ExistingFileHelper;
 import org.jetbrains.annotations.Nullable;
@@ -11,14 +14,15 @@ import org.jetbrains.annotations.Nullable;
 import java.util.concurrent.CompletableFuture;
 
 public class ModItemTagProvider extends ItemTagsProvider {
-
-
     public ModItemTagProvider(PackOutput output, CompletableFuture<HolderLookup.Provider> lookupProvider, CompletableFuture<TagLookup<Block>> blockTags, @Nullable ExistingFileHelper existingFileHelper) {
         super(output, lookupProvider, blockTags, EverythingBagel.MOD_ID, existingFileHelper);
     }
 
     @Override
     protected void addTags(HolderLookup.Provider provider) {
-
+        tag(ModItemTags.MOD_DEBUG)
+                .add(Items.STICK);
+        tag(ModItemTags.MIXING_BOWL_INPUT)
+                .add(ModItems.FLOUR.get());
     }
 }
