7cec99f9d7209f6805a67889f3ff6eeef5a90302
Edric Liu <edricdongliu@gmail.com>
2026-07-21 21:43:31 -0400

    move mix deserialization off main thread

--- src/main/java/com/nyan/everybagel/gateau/mixes/GateauMixLoader.java
+++ src/main/java/com/nyan/everybagel/gateau/mixes/GateauMixLoader.java
@@ -9,16 +9,18 @@ import com.nyan.everybagel.gateau.GateauSet;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.packs.resources.ResourceManager;
 import net.minecraft.server.packs.resources.SimpleJsonResourceReloadListener;
+import net.minecraft.server.packs.resources.SimplePreparableReloadListener;
 import net.minecraft.util.profiling.ProfilerFiller;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
-public class GateauMixLoader extends SimpleJsonResourceReloadListener {
+public class GateauMixLoader extends SimplePreparableReloadListener<Map<GateauSet, GateauSet>> {
     private static final Gson GSON = new GsonBuilder().setPrettyPrinting().disableHtmlEscaping().create();
-    private static final String folder = "everybagel/gateau_mixes";
+    private static final String FOLDER = "everybagel/gateau_mixes";
     private static final Logger LOGGER = LogManager.getLogger();
 
     public static final GateauMixLoader INSTANCE = new GateauMixLoader();
@@ -26,20 +28,33 @@ public class GateauMixLoader extends SimpleJsonResourceReloadListener {
     private Map<GateauSet, GateauSet> mixes;
 
     private GateauMixLoader() {
-        super(GSON, folder);
+        super();
         this.mixes = Map.of();
     }
 
+    private static boolean validPath(ResourceLocation path) {
+        return path.getPath().endsWith(".json");
+    }
+
     @Override
-    protected void apply(Map<ResourceLocation, JsonElement> object, ResourceManager resourceManager, ProfilerFiller profiler) {
-        HashMap<GateauSet, GateauSet> map = new HashMap<>();
-        for (Map.Entry<ResourceLocation, JsonElement> entry : object.entrySet()) {
-            ResourceLocation loc = entry.getKey();
-            JsonElement json = entry.getValue();
-            GateauMixes.CODEC.parse(JsonOps.INSTANCE, json)
-                    .resultOrPartial(onError -> LOGGER.warn("Could not parse GateauMixes with json id {}, err {}", loc, onError))
-                    .ifPresent(map::putAll);
+    protected Map<GateauSet, GateauSet> prepare(ResourceManager resourceManager, ProfilerFiller profilerFiller) {
+        var map = new HashMap<GateauSet, GateauSet>();
+        for (var entry : resourceManager.listResources(FOLDER, GateauMixLoader::validPath).entrySet()) {
+            try (var reader = entry.getValue().openAsReader()) {
+                var element = GSON.fromJson(reader, JsonElement.class);
+                GateauMixes.CODEC.parse(JsonOps.INSTANCE, element)
+                        .resultOrPartial(onError -> LOGGER.warn("Could not parse GateauMixes with json id {}, err {}", entry.getKey(), onError))
+                        .ifPresent(map::putAll);
+            }
+            catch (IOException e) {
+                LOGGER.warn("Could not parse GateauMixes with json id {}", entry.getKey(), e);
+            }
         }
+        return ImmutableMap.copyOf(map);
+    }
+
+    @Override
+    protected void apply(Map<GateauSet, GateauSet> map, ResourceManager resourceManager, ProfilerFiller profilerFiller) {
         this.mixes = map;
     }
 
